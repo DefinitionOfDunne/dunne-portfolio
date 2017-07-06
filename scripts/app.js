578253ba323eca19d1fded5b8b5a39d9ad342a23
Dylan Dunne <dylanadunne@gmail.com>
2017-07-06 18:33:09 -0400

    added image row

--- scripts/app.js
+++ scripts/app.js
@@ -5,5 +5,5 @@
    var firstName = $('input[name=firstname]').val();
    var lastName = $('input[name=lastname]').val();
    var inquiry = $('textarea[name=subject]').val();
-   $('.list').append("<div class='item'>" + 'Message for Dylan:' + "<br/>" + "From: " + firstName + " " + lastName + '<br/>'+ inquiry + "</div>");
+   $('.inquiry-all').append("<div class='inquiry-one'>" + 'Message for Dylan:' + "<br/>" + "From: " + firstName + " " + lastName + '<br/>' + '<br/>' + inquiry + "</div>");
  });
